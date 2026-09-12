30bdcc14becbccc07b6b5d4ed22e7aeb43638354
S RAMANADHA REDDY <ramnathreddy838@gmail.com>
2026-09-12 10:28:58 +0530

    Bulk sync: 3 submissions

---
+++ Data Structures & Algorithms/duplicate-integer/submission-0.py
@@ -0,0 +1,10 @@
+class Solution:
+    def hasDuplicate(self, nums: List[int]) -> bool:
+        h={}
+        for i in nums:
+            if i in h:
+                return True
+            else:
+                h[i]=1
+        return False
+        
